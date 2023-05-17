311bd72be60a345ea748d8e8fecba8ae30471f18
Łukasz Wójcik <lukasz.wojcik.22@gmail.com>
2023-05-17 15:49:33 +0200

    Generalize the compilation script, fix TOC

--- thesis/compile_thesis.sh
+++ thesis/compile_thesis.sh
@@ -1,10 +1,11 @@
 FILE=thesis.tex
+
 if [ -f "$FILE" ]; then
   mkdir -p pdf_out
-  pdflatex -output-directory=./pdf_out thesis.tex
-  bibtex pdf_out/thesis.aux
-  pdflatex -output-directory=./pdf_out thesis.tex
-  pdflatex -output-directory=./pdf_out thesis.tex
+  pdflatex -output-directory=./pdf_out $FILE
+  bibtex pdf_out/${FILE:0:-4}.aux
+  pdflatex -output-directory=./pdf_out $FILE
+  pdflatex -output-directory=./pdf_out $FILE
 else
   echo "$FILE does not exist. Ensure the directory is correct."
 fi
